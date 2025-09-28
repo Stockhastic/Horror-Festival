4dc6988678be672703c797ec909ff43932f1146c
Stockhastic <stockhastic@yandex.ru>
2025-09-28 17:39:30 +0400

    Add hover sound effects to the project
    - Added hover.ogg sound effect from free-sound-effects.net
    - Added hover2.ogg sound effect from free-sound-effects.net

--- scripts-js-php/script.js
+++ scripts-js-php/script.js
@@ -1,1 +1,33 @@
-//Java Script Here 
+const sound1 = document.querySelector('#hoverSound');
+const sound2 = document.querySelector('#hoverSound2');
+
+function playSound(sound) {
+    sound.muted = false;
+    sound.pause();
+    sound.currentTime = 0;
+    sound.play().catch(() => { });
+}
+
+// ссылки и li
+// только ссылки
+document.querySelectorAll('.sidebar__link')
+    .forEach(el => el.addEventListener('mouseenter', () => playSound(sound1)));
+
+// аватарки
+document.querySelectorAll('.sidebar__participant-avatar')
+    .forEach(el => el.addEventListener('mouseenter', () => playSound(sound2)));
+
+
+function playCustomSound(src) {
+    const audio = new Audio(src);   // создаём новый плеер
+    audio.play().catch(() => { });
+}
+
+// выбираем все аватарки
+document.querySelectorAll('.sidebar__participant-avatar')
+    .forEach(avatar => {
+        avatar.addEventListener('click', () => {
+            const soundSrc = avatar.dataset.sound; // читаем data-sound
+            if (soundSrc) playCustomSound(soundSrc);
+        });
+    });
